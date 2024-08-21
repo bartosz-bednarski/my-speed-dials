8079d4f61f72f7d25d2037fbb32c891508de592d
Bartosz Bednarski <bambocha1997@gmail.com>
2024-08-21 12:52:54 +0200

    New dials.

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -13,12 +13,31 @@ export const Icons = [
     url: "https://github.com/bartosz-bednarski",
   },
   { icon: "LinkedIn", title: "LinkedIn", url: "https://www.linkedin.com/" },
+  {
+    icon: "Jira",
+    title: "Jira",
+    url: "https://geometa.atlassian.net/jira/software/projects/FNW/boards/6",
+  },
   { icon: "Behance", title: "Behance", url: "https://www.behance.com/" },
   { icon: "Dribble", title: "Dribble", url: "https://www.dribble.com/" },
   { icon: "Pinterest", title: "Pinterest", url: "https://www.pinterest.com/" },
   { icon: "FilmWeb", title: "FilmWeb", url: "https://www.filmWeb.pl/" },
   { icon: "Ekino", title: "Ekino", url: "https://ekino-tv.pl/" },
   { icon: "HboMax", title: "HboMax", url: "https://play.max.com/" },
+  {
+    icon: "PrimeVideo",
+    title: "Prime Video",
+    url: "https://www.primevideo.com/",
+  },
+  { icon: "RmfMaxx", title: "RMFMaxx", url: "https://www.rmfmaxx.pl/" },
+  { icon: "Amazon", title: "Amazon", url: "https://www.amazon.pl/" },
+
+  {
+    icon: "CyberFolks",
+    title: "CyberFolks",
+    url: "https://cyberfolks.pl/logowanie-panel-klienta/",
+  },
+  { icon: "ChatGpt", title: "ChatGpt", url: "https://chatgpt.com/" },
   {
     icon: "GoogleSearchConsole",
     title: "Google Search Console",
